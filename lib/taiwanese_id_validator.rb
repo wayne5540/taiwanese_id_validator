1666bbf4fa8d1c2b48bed3e156e1081cca01ebf3
wayne <wayne.5540@gmail.com>
2015-12-17 13:56:36 +0800

    add allow_nil, allow_blank, case_sensitive support

--- lib/taiwanese_id_validator.rb
+++ lib/taiwanese_id_validator.rb
@@ -2,8 +2,20 @@ require "taiwanese_id_validator/twid_validator"
 
 class TaiwaneseIdValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless TwidValidator.valid?(value)
+    return if skip_check?
+
+    unless TwidValidator.valid?(value, case_sensitive?)
       record.errors[attribute] << (options[:message] || "is not an valid ID")
     end
+  end
+
+  private
+
+  def skip_check?
+    options[:allow_nil].present? || options[:allow_blank].present?
+  end
+
+  def case_sensitive?
+    options[:case_sensitive].nil? ? true : options[:case_sensitive]
   end
 end
--- spec/taiwanese_id_validator_spec.rb
+++ spec/taiwanese_id_validator_spec.rb
@@ -4,6 +4,14 @@ class TestUser < TestModel
   validates :twid, taiwanese_id: true
 end
 
+class TestUserAllowsNil < TestModel
+  validates :twid, taiwanese_id: { allow_nil: true }
+end
+
+class TestUserNoCaseSensitive < TestModel
+  validates :twid, taiwanese_id: { case_sensitive: false }
+end
+
 describe TaiwaneseIdValidator do
   describe "validation" do
     context "given the valid IDs" do
@@ -13,15 +21,24 @@ describe TaiwaneseIdValidator do
         it "#{twid} should be valid" do
           expect(TestUser.new(twid: twid)).to be_valid
         end
+
+        it "#{twid} should be valid" do
+          expect(TestUserAllowsNil.new(twid: nil)).to be_valid
+        end
+
+        it "#{twid} should be valid" do
+          expect(TestUserNoCaseSensitive.new(twid: twid.downcase)).to be_valid
+        end
       end
     end
 
     context "given the invalid IDs" do
       [
         "Y144",
-        "y1447134111",
-        "ya44713411",
-        "y144713410"
+        "Y1447134111",
+        "Ya44713411",
+        "Y144713410",
+        "y144713411"
       ].each do |twid|
         it "#{twid} should be valid" do
           expect(TestUser.new(twid: twid)).not_to be_valid
