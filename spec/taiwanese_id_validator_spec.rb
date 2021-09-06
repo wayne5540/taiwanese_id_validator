14323dc826e249b442ef5533f19616d5e62f5c18
oscar <oscarada87@gmail.com>
2021-09-06 16:45:59 +0800

    update spec class name & fix spec

--- spec/taiwanese_id_validator_spec.rb
+++ spec/taiwanese_id_validator_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-class TestUser < TestModel
+class TwTestUser < TestModel
   validates :twid, taiwanese_id: true
 end
 
-class TestUserAllowsNil < TestModel
+class TwTestUserAllowsNil < TestModel
   validates :twid, taiwanese_id: { allow_nil: true }
 end
 
-class TestUserNoCaseSensitive < TestModel
+class TwTestUserNoCaseSensitive < TestModel
   validates :twid, taiwanese_id: { case_sensitive: false }
 end
 
@@ -19,15 +19,15 @@ describe TaiwaneseIdValidator do
         "Y144713411"
       ].each do |twid|
         it "#{twid} should be valid" do
-          expect(TestUser.new(twid: twid)).to be_valid
+          expect(TwTestUser.new(twid: twid)).to be_valid
         end
 
         it "#{twid} should be valid" do
-          expect(TestUserAllowsNil.new(twid: nil)).to be_valid
+          expect(TwTestUserAllowsNil.new(twid: nil)).to be_valid
         end
 
         it "#{twid} should be valid" do
-          expect(TestUserNoCaseSensitive.new(twid: twid.downcase)).to be_valid
+          expect(TwTestUserNoCaseSensitive.new(twid: twid.downcase)).to be_valid
         end
       end
     end
@@ -40,8 +40,8 @@ describe TaiwaneseIdValidator do
         "Y144713410",
         "y144713411"
       ].each do |twid|
-        it "#{twid} should be valid" do
-          expect(TestUser.new(twid: twid)).not_to be_valid
+        it "#{twid} should not be valid" do
+          expect(TwTestUser.new(twid: twid)).not_to be_valid
         end
       end
     end
